8d5de96c0961a9942b32a17ca9f2fb0049962767
sky <rana.akash0003@gmail.com>
2020-04-28 03:25:09 +0600

    chat app created updated cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
+const cors = require("cors");
+
 const { addUser, removeUser, getUser, getUserInRoom } = require("./user");
 
 const PORT = process.env.PORT || 5000;
@@ -9,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(cors());
+app.use(router);
 
 io.on("connection", (socket) => {
   // console.log("We have a new connection!!!");
@@ -40,7 +45,7 @@ io.on("connection", (socket) => {
     console.log("########", user);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
-  
+
     // io.to(user.room).emit("roomData", {
     //   room: user.room,
     //   users: getUserInRoom(user.room),
@@ -59,7 +64,5 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on ${PORT}`));
